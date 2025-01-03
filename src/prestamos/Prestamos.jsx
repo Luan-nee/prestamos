646930b4a3a3c35dd9f0cf28ff163a6276620867
Luan-nee <luandelsol54@gmail.com>
2025-01-03 12:17:20 -0500

    relizando ajustes al diseño

--- src/prestamos/Prestamos.jsx
+++ src/prestamos/Prestamos.jsx
@@ -7,27 +7,28 @@ function Prestamos() {
       <table className="w-full bg-primary-100 divide-y">
         <thead>
           <tr>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              
+            <th scope="col" className="text-center py-2 text-sm font-medium text-gray-500 uppercase tracking-wider">
+              {/* Columna de los checkbox */}
             </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" className="text-center py-2 text-sm font-medium text-gray-500 uppercase tracking-wider">
               Fecha
             </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" className="text-center py-2 text-sm font-medium text-gray-500 uppercase tracking-wider">
               Producto
             </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" className="text-center py-2 text-sm font-medium text-gray-500 uppercase tracking-wider">
               Adelanto
             </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" className="text-center py-2 text-sm font-medium text-gray-500 uppercase tracking-wider">
               Costo
             </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" className="text-center py-2 text-sm font-medium text-gray-500 uppercase tracking-wider">
               Monto a Pagar
             </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              
+            <th scope="col" className="text-center py-2 text-sm font-medium text-gray-500 uppercase tracking-wider">
+              {/* Columan de los botones Ver y Eliminar */}
             </th>
+            
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
--- src/prestamos/components/Registros.jsx
+++ src/prestamos/components/Registros.jsx
@@ -20,35 +20,37 @@ function Registros() {
     return data.sort((a, b) => a.pagado - b.pagado);
   };
 
+  ordenar(prestamosData);
+
   return (
     <>
       {prestamos.map((prestamo, index) => (
         <tr key={index}>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            <input
-              type="checkbox"
-              className="form-checkbox h-5 w-5 text-blue-600"
-              checked={prestamo.pagado}
-              onChange={() => handleCheckboxChange(index)}
-            />
+          <td className="flex flex-row justify-center whitespace-nowrap text-sm text-gray-500">
+            <div>
+              <input
+                type="checkbox"
+                className="form-checkbox h-5 w-5 text-blue-600"
+                checked={prestamo.pagado}
+                onChange={() => handleCheckboxChange(index)}
+              />
+            </div>
           </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{prestamo.fecha}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{prestamo.nombreDelProducto}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">S/{prestamo.adelanto}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">S/{prestamo.costo}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">S/{prestamo.montoAPagar}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+          <td className="whitespace-nowrap text-center text-sm text-gray-500">{prestamo.fecha}</td>
+          <td className="whitespace-nowrap text-center text-sm text-gray-500">{prestamo.nombreDelProducto}</td>
+          <td className="whitespace-nowrap text-center text-sm text-gray-500">S/{prestamo.adelanto}</td>
+          <td className="whitespace-nowrap text-center text-sm text-gray-500">S/{prestamo.costo}</td>
+          <td className="whitespace-nowrap text-center text-sm text-gray-500">S/{prestamo.montoAPagar}</td>
+          <td className="flex flex-row justify-center whitespace-nowrap text-sm text-gray-500">
             <div className="flex flex-row gap-1">
               <button className="btn p-1 bg-blue-500 text-white rounded-md">
                 <Ojo width={30} />
               </button>
               { 
-                prestamo.pagado ? (
+                prestamo.pagado && (
                   <button className="btn p-1 bg-red-500 text-white rounded-md">
                     <Delete width={30} />
                   </button>
-                ) : (
-                  <span></span>
                 )
               }
             </div>
